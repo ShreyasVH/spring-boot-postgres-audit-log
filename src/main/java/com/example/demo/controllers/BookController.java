d6744f6c712f3c5e119742adf46b1c4c91751a79
ShreyasVH <shreyas.deve.1@gmail.com>
2023-12-01 17:08:53 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controllers/BookController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.db.Book;
+import com.example.demo.models.requests.CreateRequest;
+import com.example.demo.models.requests.UpdateRequest;
+import com.example.demo.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BookController
+{
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/v1/books")
+    public Book create(@RequestBody CreateRequest request)
+    {
+        return this.bookService.create(request);
+    }
+
+    @GetMapping("/v1/books")
+    public List<Book> list()
+    {
+        return this.bookService.list();
+    }
+
+    @GetMapping("/v1/books/{id}")
+    public Book get(@PathVariable Long id) {
+        return this.bookService.get(id);
+    }
+
+    @PutMapping("/v1/books/{id}")
+    public Book update(@PathVariable Long id, @RequestBody UpdateRequest request) {
+        return this.bookService.update(id, request);
+    }
+
+    @DeleteMapping("/v1/books/{id}")
+    public String delete(@PathVariable Long id) {
+        this.bookService.delete(id);
+        return "Success";
+    }
+}
